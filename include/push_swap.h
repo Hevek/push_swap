e6e368484e5d4bf1049bb0650e2d34ec0d7d8bad
restevez <restevez@student.42sp.org.br>
2025-04-18 19:46:18 -0300

    push_swap - Day Three - v0

---
+++ include/push_swap.h
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: restevez <restevez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 19:06:55 by restevez          #+#    #+#             */
+/*   Updated: 2025/04/18 19:39:43 by restevez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdbool.h> //To use bool flags, e.g, to print or not to print
+# include <limits.h> //To define MIN and MAX macros
+/* # include "../ft_printf/libft/libft.h"
+# include "../ft_printf/ft_printf.h" */
+
+// A container of data enclosed in {} braces. `s_` for struct
+typedef struct s_stack_node
+{
+	int					nbr; //The number to sort
+	int					index; //The number's position in the stack
+	int					push_cost; //How many commands in total
+	bool				above_median; //Used to calculate `push_cost`
+	bool				cheapest; //The node that is the cheapest to do commands
+	struct s_stack_node	*target_node; // Target node of a node in the op. stack
+	struct s_stack_node	*next; //A pointer to the next node
+	struct s_stack_node	*prev; //A pointer to the previous node
+}	t_stack_node; //The "shortened name", "t_stack_node". `t_` for type
+
+//***Handle errors
+int				error_syntax(char *str_n);
+int				error_duplicate(t_stack_node *a, int n);
+void			free_stack(t_stack_node **stack);
+void			free_errors(t_stack_node **a);
+
+//***Stack initiation
+void			init_stack_a(t_stack_node **a, char **argv);
+char			**split(char *s, char c);
+
+//***Nodes initiation
+void			init_nodes_a(t_stack_node *a, t_stack_node *b);
+void			init_nodes_b(t_stack_node *a, t_stack_node *b);
+void			current_index(t_stack_node *stack);
+void			set_cheapest(t_stack_node *stack);
+t_stack_node	*get_cheapest(t_stack_node *stack);
+void			prep_for_push(t_stack_node **s, t_stack_node *n, char c);
+
+//***Stack utils
+int				stack_len(t_stack_node *stack);
+t_stack_node	*find_last(t_stack_node *stack);
+bool			stack_sorted(t_stack_node *stack);
+t_stack_node	*find_min(t_stack_node *stack);
+t_stack_node	*find_max(t_stack_node *stack);
+
+//***Commands
+void			sa(t_stack_node **a, bool print);
+void			sb(t_stack_node **b, bool print);
+void			ss(t_stack_node **a, t_stack_node **b, bool print);
+void			ra(t_stack_node **a, bool print);
+void			rb(t_stack_node **b, bool print);
+void			rr(t_stack_node **a, t_stack_node **b, bool print);
+void			rra(t_stack_node **a, bool print);
+void			rrb(t_stack_node **b, bool print);
+void			rrr(t_stack_node **a, t_stack_node **b, bool print);
+void			pa(t_stack_node **a, t_stack_node **b, bool print);
+void			pb(t_stack_node **b, t_stack_node **a, bool print);
+
+//***Algorithm
+void			sort_three(t_stack_node **a);
+void			sort_stacks(t_stack_node **a, t_stack_node **b);
+
+#endif
